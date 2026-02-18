ebb8e1643144900d35b306185a6cf7f133bea152
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-02-17 20:15:50 -0600

    Create 693_Binary_Number_with_Alternating_Bits.cpp

---
+++ 17feb2026/693_Binary_Number_with_Alternating_Bits.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/binary-number-with-alternating-bits/description/?envType=daily-question&envId=2026-02-18
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = n & 1;
+        n >>= 1;
+        while (n) {
+            int curr = n & 1;
+            if (curr == 0 && prev == 0) return false;
+            if (curr == 1 && prev == 1) return false;
+            n >>= 1;
+            prev = curr;
+        }
+        return true;
+    }
+};
