dd7dd401a5d6fbac0b2a11a491b694c240968181
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-03-08 19:45:19 -0500

    Create solution.cpp

---
+++ prep_list/1-topic-arrays-strings/4-prefix/1352-product-of-the-last-k-numbers/solution.cpp
@@ -0,0 +1,25 @@
+class ProductOfNumbers {
+public:
+    ProductOfNumbers() {}
+    
+    void add(int num) {
+        if (num == 0) {
+            product = vector<int>();
+        }
+        else {
+            int x = (product.size() > 0) ? product.back() * num : num;
+            product.push_back(x);
+        }
+    }
+    
+    int getProduct(int k) {
+        int n = product.size();
+        if (n >= k) { 
+            return (n - k - 1 >= 0) ? product.back() / product[n - k - 1] : product.back();
+        }
+        return 0;
+    }
+
+private:
+    vector<int> product;
+};
